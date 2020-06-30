c15708e7b29787cfa67e73ca193ee63d7d3295b8
pato <pato.araya.gonzalez@gmail.com>
2020-06-30 11:05:35 -0400

    feat(scrapper): add scrapper module

---
+++ scrapper/src/main/java/cl/ucn/disc/pdis/scrapper/Contact.java
@@ -0,0 +1,82 @@
+/*
+ * The MIT License (MIT)
+ * Copyright (c) 2020 Patricio Araya
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ * software and associated documentation files (the “Software”), to deal in the Software
+ * without restriction, including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
+ * to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package cl.ucn.disc.pdis.scrapper;
+
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+@DatabaseTable(tableName = "contactos")
+public class Contact {
+
+    @DatabaseField(id = true)
+    private Integer id;
+    @DatabaseField
+    private String name;
+    @DatabaseField
+    private String position;
+    @DatabaseField
+    private String unit;
+    @DatabaseField
+    private String email;
+    @DatabaseField
+    private String phone;
+    @DatabaseField
+    private String office;
+    @DatabaseField
+    private String address;
+
+    /**
+     * ORMlite constructor
+     */
+    public Contact(){
+        // ORM lite needs an no-arg constructor
+    }
+
+    /**
+     * Constructor
+    */
+    public Contact(Integer id, String name, String position, String unit, String email, String phone, String office, String address) {
+        this.id = id;
+        this.name = name;
+        this.position = position;
+        this.unit = unit;
+        this.email = email;
+        this.phone = phone;
+        this.office = office;
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", position='" + position + '\'' +
+                ", unit='" + unit + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", office='" + office + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
---
+++ scrapper/src/main/java/cl/ucn/disc/pdis/scrapper/Scrapper.java
@@ -0,0 +1,148 @@
+/*
+ * The MIT License (MIT)
+ * Copyright (c) 2020 Patricio Araya
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ * software and associated documentation files (the “Software”), to deal in the Software
+ * without restriction, including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
+ * to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package cl.ucn.disc.pdis.scrapper;
+
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
+import com.j256.ormlite.jdbc.JdbcConnectionSource;
+import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Random;
+
+public class Scrapper {
+
+    /** Logger */
+    public static Logger log = LoggerFactory.getLogger(Scrapper.class);
+
+    /**
+     * Main method
+     * @param args .
+     */
+    public static void main(String[] args){
+
+        // Setup DB
+        String dbUrl = "jdbc:sqlite:contactos.db";
+        ConnectionSource connectionSource = null;
+        Dao<Contact,Integer> contactDao = null;
+
+        try {
+            connectionSource = new JdbcConnectionSource(dbUrl);
+            log.debug("DB conection open");
+            // Create the DAO
+            contactDao = DaoManager.createDao(connectionSource, Contact.class);
+            // Create table;
+            TableUtils.createTableIfNotExists(connectionSource, Contact.class);
+
+        } catch (SQLException e){
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            log.error("Error creating a DB connection: "+ e.getMessage());
+        } finally {
+            try {
+                assert connectionSource != null;
+                connectionSource.close();
+            } catch (IOException e) {
+                log.error("Error closing DB connection: "+e.getMessage());
+            }
+        }
+
+        // Search for last id in db and start scrapping from it.
+        int id = 0;
+        try {
+            assert contactDao != null;
+            String strId = contactDao.queryRaw("select max(id) from contactos").getFirstResult()[0];
+            try{
+            id = Integer.parseInt(strId);
+            } catch (Exception e){
+                log.debug("Error parsing id. Empty db?: "+e.getMessage());
+            }
+            log.info("Starting scrapping from ID: "+id);
+        } catch (SQLException e){
+            log.error("Error getting last iserted id: "+e.getMessage());
+        }
+
+        int lastId = 29730;
+
+        // For every id get contact info
+        for(; id <=lastId; id++){
+            log.info("Getting contact id: "+id);
+            Contact contact = getContactInfo(id);
+            if(contact != null){
+                try{
+                    contactDao.create(contact);
+                } catch (SQLException e){
+                    log.error("Error inserting contact info:"+ e.getMessage());
+                }
+            }
+
+            try {
+                Random random = new Random();
+                int delay = 500 + random.nextInt(500);
+                Thread.sleep(delay);
+                log.info("Delay of :"+ delay);
+            } catch (InterruptedException e){
+                log.debug("Delay was interrupted: "+e.getMessage());
+            }
+
+        }
+
+    }
+
+    private static Contact getContactInfo(Integer id){
+        final String url = "http://online.ucn.cl/directoriotelefonicoemail/fichaGenerica/?cod=";
+        Contact newContact = null;
+        try{
+            Document doc = Jsoup.connect(url+id).get();
+            String name =  doc.getElementById("lblNombre").text();
+            String position =  doc.getElementById("lblCargo").text();
+            String unit =  doc.getElementById("lblUnidad").text();
+            String email =  doc.getElementById("lblEmail").text();
+            String phone =  doc.getElementById("lblTelefono").text();
+            String office =  doc.getElementById("lblOficina").text();
+            String address =  doc.getElementById("lblDireccion").text();
+
+            if(!name.isEmpty()){
+                newContact = new Contact(id,name,position,unit,email,phone,office,address);
+                log.debug("CONTACT: "+ newContact.toString());
+            }
+
+
+        } catch (IOException e) {
+            log.error("Error retrieving contact info: "+e.getMessage());
+        }
+
+        return newContact;
+
+    }
+
+
+
+
+    
+}
