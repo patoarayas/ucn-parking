7f6a045de9e3b872bf97f036dbfb407a94b02a1c
pato <pato.araya.gonzalez@gmail.com>
2020-07-11 18:37:04 -0400

    feat(scrapper): update db with rut

--- scrapper/src/main/java/cl/ucn/disc/pdis/scrapper/Scrapper.java
+++ scrapper/src/main/java/cl/ucn/disc/pdis/scrapper/Scrapper.java
@@ -77,6 +77,7 @@ public class Scrapper {
 
     // Searches for the last id inside the DB and starts scrapping from it.
     int lastId = 0;
+
     try {
       assert contactDao != null;
 
@@ -98,8 +99,9 @@ public class Scrapper {
     }
 
     // Gets the contact's info by each ID and then creates it.
-    int maxId = 30000;
+    int maxId = 40000;
     for (int id = lastId; id <= maxId; id++) {
+
       log.info("Getting contact's id: {}", id);
 
       // Contact's info.
@@ -212,5 +214,6 @@ public class Scrapper {
 
     return contact;
   }
+
 
 }
