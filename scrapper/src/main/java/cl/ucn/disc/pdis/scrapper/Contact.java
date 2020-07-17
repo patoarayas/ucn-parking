feb7b092dcb5ab34e173947e1e2152c7b1c95bd6
WsaaP <ariel.vejar@live.cl>
2020-07-17 05:55:21 -0400

    refactor(Contact): add get's methods

--- scrapper/src/main/java/cl/ucn/disc/pdis/scrapper/Contact.java
+++ scrapper/src/main/java/cl/ucn/disc/pdis/scrapper/Contact.java
@@ -3,7 +3,7 @@
  * Copyright (c) 2020 Patricio Araya, David Canto, Ariel Vejar
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this
- * software and associated documentation files (the “Software”), to deal in the Software
+ * software and associated documentation files (the "Software"), to deal in the Software
  * without restriction, including without limitation the rights to use, copy, modify, merge,
  * publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
  * to whom the Software is furnished to do so, subject to the following conditions:
@@ -11,7 +11,7 @@
  * The above copyright notice and this permission notice shall be included in all copies or
  * substantial portions of the Software.
  *
- * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
  * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
@@ -123,7 +123,6 @@ public class Contact {
     this.rut = null;
     this.gender = null;
     this.city = stripCity(address);
-
   }
 
   public Integer getCod() {
@@ -136,6 +135,14 @@ public class Contact {
 
   public String getRut() {
     return rut;
+  }
+
+  public String getGender() {
+    return gender;
+  }
+
+  public String getEmail() {
+    return email;
   }
 
   public void setRut(String rut) {
@@ -169,7 +176,5 @@ public class Contact {
   public String toString() {
     return ReflectionToStringBuilder.toString(this);
   }
-
-
 }
 
