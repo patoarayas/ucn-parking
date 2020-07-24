0db04191a78daf9b97b7c285f32a351d668a7755
tigerflight <david.canto@alumnos.ucn.cl>
2020-07-24 10:41:44 -0400

    feat(laravel): Updated controller for testing connection with Ice

--- web/app/Http/Controllers/ConnectionController.php
+++ web/app/Http/Controllers/ConnectionController.php
@@ -1,18 +1,46 @@
 <?php
-
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
-
+require 'Ice.php';
+require 'test.ice';
 class ConnectionController
 {
-    public function connection(){
+    public function connection()
+    {
         return view('connection');
     }
 
-    public function receive(Request $request){
+    public function receive(Request $request)
+    {
         $data = $request->input('testData');
-        var_dump($data);
+        if ($data != null){
+            $timeNow = $data;
+        }else{
+            $timeNow = date("h:i:s");
+        }
+
+        $ic = null;
+        try
+        {
+            $ic = Ice\initialize();
+            $base = $ic->stringToProxy("TestConnection:default -p 3000");
+            $connection = model\SistemaHelper::checkedCast($base);
+            if(!$connection)
+            {
+                throw new RuntimeException("Invalid proxy");
+            }
+
+            $connection->getDelay($data);
+        }
+        catch(Exception $ex)
+        {
+            echo $ex;
+        }
+
+        if($ic)
+        {
+            $ic->destroy(); // Clean up
+        }
     }
 }
+?>
