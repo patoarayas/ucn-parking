64064b09c764e2e1955639fb708925c8812e4342
pato <pato.araya.gonzalez@gmail.com>
2020-08-05 15:48:05 -0400

    chore(app): add copyright notice

--- app/app/src/main/java/cl/ucn/disc/pdis/parking/ZeroIce.java
+++ app/app/src/main/java/cl/ucn/disc/pdis/parking/ZeroIce.java
@@ -1,3 +1,26 @@
+/*
+ * Copyright (c) 2020. Patricio Araya, David Canto, Ariel Vejar.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software
+ * is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ *
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package cl.ucn.disc.pdis.parking;
 
 import com.zeroc.Ice.Communicator;
@@ -15,82 +38,64 @@ import static com.zeroc.Ice.Util.createProperties;
 
 public class ZeroIce {
 
-    // Logger
-    private static final Logger log = LoggerFactory.getLogger(ZeroIce.class);
+  // Logger
+  private static final Logger log = LoggerFactory.getLogger(ZeroIce.class);
 
-    // Singleton instance
-    private static final ZeroIce ZERO_ICE = new ZeroIce();
+  // Singleton instance
+  private static final ZeroIce ZERO_ICE = new ZeroIce();
 
-    // Communicator
-    private Communicator communicator;
+  // Communicator
+  private Communicator communicator;
 
-    // ContratosImpl
-    ContratosPrx contratosPrx;
+  // ContratosImpl
+  ContratosPrx contratosPrx;
 
-    // SistemaImpl
-    SistemaPrx sistemaPrx;
+  // SistemaImpl
+  SistemaPrx sistemaPrx;
 
-    /**
-     * Empty constructor.
-     */
-    public ZeroIce(){
+  /**
+   * Empty constructor.
+   */
+  public ZeroIce() {
 
+  }
+
+  /**
+   * Get singleton instance
+   *
+   * @return Instance
+   */
+  public static ZeroIce getInstance() {
+    return ZERO_ICE;
+  }
+
+
+  /**
+   * Start communication
+   */
+  public void start() {
+    if (this.communicator != null) {
+      log.warn("Communicator was already initialized");
+      return;
     }
 
-    /**
-     * Get singleton instance
-     * @return Instance
-     */
-    public static ZeroIce getInstance(){
-        return ZERO_ICE;
+    this.communicator = Util.initialize();
+
+    this.contratosPrx = ContratosPrx.checkedCast(this.communicator.stringToProxy("Contratos:tcp -z -t 15000 -p 3000"));
+    this.sistemaPrx = SistemaPrx.checkedCast(this.communicator.stringToProxy("Sistema:tcp -z -t 15000 -p 3000"));
+    log.debug("Communication started");
+  }
+
+  /**
+   * Stop communication
+   */
+  public void stop() {
+    if (this.communicator == null) {
+      log.warn("Communicator already stopped");
+      return;
     }
-
-    /**
-     * @param args to use as source.
-     * @return the {@link InitializationData}.
-     */
-    private static InitializationData getInitializationData(String[] args) {
-
-
-        // Properties
-        final Properties properties = createProperties(args);
-        properties.setProperty("Ice.Package.model", "cl.ucn.disc.pdis.parking.zeroice");
-        //properties.setProperty("Ice.Plugin.Slf4jLogger.java", "cl.ucn.disc.pdis.fivet.zeroice.Slf4jLoggerPluginFactory");
-
-        InitializationData initializationData = new InitializationData();
-        initializationData.properties = properties;
-
-        return initializationData;
-    }
-
-    /**
-     * Start communication
-     */
-    public void start(){
-        if(this.communicator != null){
-            log.warn("Communicator was already initialized");
-            return;
-        }
-        //String[] args = {"1","2"};
-        //this.communicator = Util.initialize(getInitializationData(args));
-
-        this.communicator = Util.initialize();
-
-        this.contratosPrx = ContratosPrx.checkedCast(this.communicator.stringToProxy("Contratos:tcp -z -t 15000 -p 3000"));
-        this.sistemaPrx = SistemaPrx.checkedCast(this.communicator.stringToProxy("Sistema:tcp -z -t 15000 -p 3000"));
-        log.debug("Communication started");
-    }
-
-    /**
-     * Stop communication
-     */
-    public void stop(){
-        if(this.communicator == null){
-            log.warn("Communicator already stopped");
-            return;
-        }
-        this.contratosPrx = null;
-        this.communicator.destroy();
-        log.debug("Communication stopped");
-    }
+    this.contratosPrx = null;
+    this.communicator.destroy();
+    log.debug("Communication stopped");
+  }
 }
--- app/app/src/test/java/cl/ucn/disc/pdis/parking/ZeroIceTest.java
+++ app/app/src/test/java/cl/ucn/disc/pdis/parking/ZeroIceTest.java
@@ -1,3 +1,26 @@
+/*
+ * Copyright (c) 2020. Patricio Araya, David Canto, Ariel Vejar.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software
+ * is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ *
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package cl.ucn.disc.pdis.parking;
 
 import org.junit.Test;
@@ -7,21 +30,21 @@ import org.slf4j.LoggerFactory;
 import java.sql.Time;
 
 public class ZeroIceTest {
-    // Logger
-    private static final Logger log = LoggerFactory.getLogger(ZeroIceTest.class);
+  // Logger
+  private static final Logger log = LoggerFactory.getLogger(ZeroIceTest.class);
 
-    // Zero Ice instance
-    private static final ZeroIce zeroIce = ZeroIce.getInstance();
+  // Zero Ice instance
+  private static final ZeroIce zeroIce = ZeroIce.getInstance();
 
 
-    @Test
-    public void getDelayTest(){
+  @Test
+  public void getDelayTest() {
 
-        zeroIce.start();
-        long clientTime = System.currentTimeMillis();
-        long delay = zeroIce.sistemaPrx.getDelay(clientTime);
-        log.debug("Delay of: "+String.valueOf(delay)+" milliseconds");
-        zeroIce.stop();
+    zeroIce.start();
+    long clientTime = System.currentTimeMillis();
+    long delay = zeroIce.sistemaPrx.getDelay(clientTime);
+    log.debug("Delay of: " + String.valueOf(delay) + " milliseconds");
+    zeroIce.stop();
 
-    }
+  }
 }
