899a99aed329d3b972f4d3b0ba75f1637260ed7f
tigerflight <david.canto@alumnos.ucn.cl>
2020-08-21 07:49:19 -0400

    fix(web): Change validation method for user register

--- web/app/Http/Controllers/RegistrosController.php
+++ web/app/Http/Controllers/RegistrosController.php
@@ -1,16 +1,12 @@
 <?php
-
 namespace App\Http\Controllers;
-
-use App\Http\Requests;
+use App\Http\Requests\StoreUserValidate;
 use http\Url;
 use Illuminate\Http\Request;
 use model\Persona;
 use model\Vehiculo;
-use Freshwork\ChileanBundle\Rut;
-
 require_once 'Ice.php';
-require_once base_path() . '/domain.php';
+require_once base_path().'/domain.php';
 
 class RegistrosController extends Controller
 {
@@ -37,88 +33,58 @@ class RegistrosController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param Request $request
+     * @param  \app\Http\Requests\RegistrosController  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(StoreUserValidate $request)
     {
-        $personaValidatedData = $request->validate([
-            'rut' => 'required|alpha_dash',
-            'nombre' => 'required',
-            'email' => 'required|email',
-            'fono' => 'numeric',
-            'movil' => 'required|numeric',
-            'unidadAcademica' => '',
-            'rol' => 'required',
-            'genero' => 'required'
-        ]);
+        $request;
 
-        if (Rut::parse($personaValidatedData['rut'])->validate()) {
-            $personaValidatedData['rut'] = Rut::parse($personaValidatedData['rut'])->normalize();
-        } else {
-            // Invalid rut, redirect
-            return redirect('registro/create')->withErrors(['Rut invalido']);
-        }
+        $rut = $request->input('rut');
+        $nombre = $request->input('nombre');
+        $email = $request->input('email');
+        $fono = $request->input('fono');
+        $movil = $request->input('movil');
+        $unidadAcademica = $request->input('unidadAcademica');
+        $rol = intval($request->input('rol'));
+        $genero = intval($request->input('genero'));
 
-        $vehiculoValidatedData = $request->validate([
-            'patente' => 'required|alpha_num|between:5,6',
-            'marca' => 'required',
-            'modelo' => 'required',
-            'anio' => 'integer',
-            'observacion' => 'nullable',
-            'color' => 'required'
-        ]);
+        $patente = $request->input('patente');
+        $marca = $request->input('marca');
+        $modelo = $request->input('modelo');
+        $anio = intval($request->input('anio'));
+        $observacion = $request->input('observacion');
+        $color = $request->input('color');
 
-        try {
+        try
+        {
             $communicator = \Ice\Initialize();
-            // Create Persona
-            $persona = new Persona(
-                $personaValidatedData['rut'],
-                $personaValidatedData['nombre'],
-                intval($personaValidatedData['genero']),
-                $personaValidatedData['email'],
-                $personaValidatedData['fono'],
-                $personaValidatedData['movil'],
-                $personaValidatedData['unidadAcademica'],
-                intval($personaValidatedData['rol'])
-            );
-
-            // Create Vehiculo
-            $vehiculo = new Vehiculo(
-                $personaValidatedData['rut'],
-                $vehiculoValidatedData['patente'],
-                $vehiculoValidatedData['marca'],
-                $vehiculoValidatedData['modelo'],
-                intval($vehiculoValidatedData['anio']),
-                $vehiculoValidatedData['observacion'],
-                $vehiculoValidatedData['color']
-            );
-
+            $persona = new Persona($rut, $nombre, $genero, $email, $fono, $movil, $unidadAcademica, $rol);
+            $vehiculo = new Vehiculo($rut, $patente, $marca, $modelo, $anio, $observacion, $color);
             $sistema_proxy = $communicator->StringToProxy("Contratos:tcp -z -t 15000 -p 3000");
             $sistema = \model\ContratosPrxHelper::uncheckedCast($sistema_proxy);
-
             // Calls interface method
             $sistema->registrarPersona($persona);
             $sistema->registrarVehiculo($vehiculo);
 
-
-        } catch (Exception $ex) {
-            // TODO: Catch Ice exceptions
-            return redirect('registro/create')->withErrors([$ex->getMessage()]);
+            if(!$sistema_proxy)
+            {
+                throw new RuntimeException("Invalid proxy");
+            }
         }
-
-
-        return redirect('/')->with('status', 'Registro añadido correctamente');
+        catch(Exception $ex)
+        {
+            echo $ex;
+        }
     }
 
     /**
      * Display the specified resource.
      *
-     * @param int $id
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public
-    function show($id)
+    public function show($id)
     {
         //
     }
@@ -126,11 +92,10 @@ class RegistrosController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param int $id
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public
-    function edit($id)
+    public function edit($id)
     {
         //
     }
@@ -138,12 +103,11 @@ class RegistrosController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param \Illuminate\Http\Request $request
-     * @param int $id
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public
-    function update(Request $request, $id)
+    public function update(Request $request, $id)
     {
         //
     }
@@ -151,11 +115,10 @@ class RegistrosController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param int $id
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public
-    function destroy($id)
+    public function destroy($id)
     {
         //
     }
